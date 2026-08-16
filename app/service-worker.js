da75597200a3dbae4319d2c2a169b48254128f24
carlosggar-arch <carlosggar@gmail.com>
2026-08-16 10:35:44 -0400

    Renueva caché de la PWA v20

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v19";
+const CACHE_VERSION = "v20";
 const SHELL_CACHE = `agenda-cultural-shell-${CACHE_VERSION}`;
 const DATA_CACHE = `agenda-cultural-data-${CACHE_VERSION}`;
 
