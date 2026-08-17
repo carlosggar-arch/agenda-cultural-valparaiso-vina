fdeb14674f8cdec50cd795a93e5aca0c7966c187
carlosggar-arch <carlosggar@gmail.com>
2026-08-16 20:15:51 -0400

    Refresh cache after schedule observer hotfix

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v23";
+const CACHE_VERSION = "v24";
 const SHELL_CACHE = `agenda-cultural-shell-${CACHE_VERSION}`;
 const DATA_CACHE = `agenda-cultural-data-${CACHE_VERSION}`;
 
