ca468110eae309f01c359d062c837b5efae1e26c
carlosggar-arch <carlosggar@gmail.com>
2026-08-17 07:14:44 -0400

    Refresh PWA shell cache for Gijon detail source UI

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v25";
+const CACHE_VERSION = "v26";
 const SHELL_CACHE = `agenda-cultural-shell-${CACHE_VERSION}`;
 const DATA_CACHE = `agenda-cultural-data-${CACHE_VERSION}`;
 
