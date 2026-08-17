83ac41444a96d3dea0587b38603c2690ee611cdd
carlosggar-arch <carlosggar@gmail.com>
2026-08-17 07:14:31 -0400

    Bump PWA after Gijon detail-source improvement

--- app/pwa.js
+++ app/pwa.js
@@ -10,7 +10,7 @@ import "./community-source.js";
 import "./header-redesign.js";
 import "./density-polish.js";
 
-const APP_VERSION = "PWA v25";
+const APP_VERSION = "PWA v26";
 const versionNode = document.querySelector("[data-app-version]");
 if (versionNode) versionNode.textContent = APP_VERSION;
 
