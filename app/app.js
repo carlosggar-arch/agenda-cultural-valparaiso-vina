679c9b68406425e8223034c8c8fc50732354df53
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 05:27:28 -0400

    Activa galería agrupada de exposiciones

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 import "./category-normalizer.js?v=20260818-categories1";
 import "./app-core.js?v=20260818-exhibitions1";
-import "./exhibition-hours.js?v=20260818-opening-hours1";
+import "./exhibition-gallery.js?v=20260818-gallery2";
