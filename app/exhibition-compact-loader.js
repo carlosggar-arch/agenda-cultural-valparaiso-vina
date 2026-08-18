5e9a5a25db85563fa9bf25fa5ec844ad9f6bd7d9
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 06:01:51 -0400

    Carga layout seguro de exposiciones sin alturas fijas

---
+++ app/exhibition-compact-loader.js
@@ -0,0 +1,18 @@
+import "./exhibition-compact.js?v=20260818-compact2";
+
+const STYLE_ID = "exhibition-compact-styles";
+const STYLE_URL = new URL("./exhibition-compact.css?v=20260818-compact3", import.meta.url).href;
+
+function activateSafeLayout() {
+  let link = document.getElementById(STYLE_ID);
+  if (!link) {
+    link = document.createElement("link");
+    link.id = STYLE_ID;
+    link.rel = "stylesheet";
+    document.head.append(link);
+  }
+  if (link.href !== STYLE_URL) link.href = STYLE_URL;
+}
+
+activateSafeLayout();
+requestAnimationFrame(activateSafeLayout);
