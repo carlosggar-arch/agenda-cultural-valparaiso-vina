fb4070a253c9d9f66f2f311f36a068d4009582f2
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 11:46:17 -0400

    Load stability-only exhibition layout

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@ import "./category-normalizer.js?v=20260818-categories3";
 import "./app-core.js?v=20260818-exhibitions1";
 import "./exhibition-venue-grouping.js?v=20260818-venuegroup1";
 import "./exhibition-gallery.js?v=20260818-gallery2";
-import "./exhibition-compact-loader.js?v=20260818-compact9";
+import "./exhibition-compact-loader.js?v=20260818-compact-disabled1";
 import "./presentation-normalizer.js?v=20260818-presentation3";
 import "./footer-credit.js?v=20260818-footer1";
