ed3d0e5b88c06e07c5ae7e013ef70067afc98151
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 10:31:34 -0400

    Make filters and integrity guard non-blocking

--- app/combined-filters-bootstrap.js
+++ app/combined-filters-bootstrap.js
@@ -1,11 +1,6 @@
-async function waitForBaseApp() {
-  while (!globalThis.__vivamosAppBaseReady) {
-    await new Promise((resolve) => requestAnimationFrame(resolve));
-  }
-  await globalThis.__vivamosAppBaseReady;
-}
+import "./category-normalizer.js?v=20260818-categories3";
 
-await waitForBaseApp();
-await import("./category-normalizer.js?v=20260818-categories3");
-await import("./combined-filters.js?v=20260818-public-taxonomy1");
-await import("./approved-event-integrity.js?v=20260818-integrity1");
+const filtersReady = import("./combined-filters.js?v=20260818-public-taxonomy1");
+filtersReady
+  .then(() => import("./approved-event-integrity.js?v=20260818-integrity1"))
+  .catch((error) => console.error("¡Vivamos!: secondary filter layer failed", error));
