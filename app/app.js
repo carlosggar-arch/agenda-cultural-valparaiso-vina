2ee4a5519af711331ea2ede97ff9e1e3916c247f
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 06:02:02 -0400

    Activa layout de tarjetas sin recortes

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
 import "./category-normalizer.js?v=20260818-categories1";
 import "./app-core.js?v=20260818-exhibitions1";
 import "./exhibition-gallery.js?v=20260818-gallery2";
-import "./exhibition-compact.js?v=20260818-compact2";
+import "./exhibition-compact-loader.js?v=20260818-compact3";
