b2ab592e15c91460e97627f734d33f1632ac50f9
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 06:42:47 -0400

    Activa normalización visual de títulos y categorías

--- app/app.js
+++ app/app.js
@@ -3,3 +3,4 @@ import "./app-core.js?v=20260818-exhibitions1";
 import "./exhibition-venue-grouping.js?v=20260818-venuegroup1";
 import "./exhibition-gallery.js?v=20260818-gallery2";
 import "./exhibition-compact-loader.js?v=20260818-compact6";
+import "./presentation-normalizer.js?v=20260818-presentation1";
