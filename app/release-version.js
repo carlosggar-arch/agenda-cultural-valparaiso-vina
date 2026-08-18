d6861fdd537fb4c765eb218f18eee5bd86c20928
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 18:12:21 -0400

    Bump PWA release to v107

--- app/release-version.js
+++ app/release-version.js
@@ -1,5 +1,5 @@
 (() => {
   // Single source of truth for the public PWA release and service-worker cache.
-  const RELEASE = 106;
+  const RELEASE = 107;
   globalThis.__VIVAMOS_RELEASE__ = RELEASE;
 })();
