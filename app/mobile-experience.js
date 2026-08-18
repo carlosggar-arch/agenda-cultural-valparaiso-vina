cafb04d7ded5ba8b0138f05edde7c7b7f1fb2134
carlosggar-arch <carlosggar@gmail.com>
2026-08-17 20:24:47 -0400

    Bump mobile controls visibility asset

--- app/mobile-experience.js
+++ app/mobile-experience.js
@@ -21,7 +21,7 @@ function installMobileMetadata() {
 }
 
 function installMobileStyles() {
-  const href = "./mobile-experience.css?v=20260817-topcontrols2";
+  const href = "./mobile-experience.css?v=20260817-topcontrols3";
   const links = [...document.querySelectorAll('link[href*="mobile-experience.css"]')];
   if (links.length) {
     links[0].href = href;
