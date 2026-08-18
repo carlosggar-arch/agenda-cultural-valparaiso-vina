de353cccdccd62bc0ba4692c374ead1691050808
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 09:34:22 -0400

    Activa taxonomía refinada, fallbacks y créditos

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
-import "./category-normalizer.js?v=20260818-categories2";
+import "./category-normalizer.js?v=20260818-categories3";
 import "./app-core.js?v=20260818-exhibitions1";
 import "./exhibition-venue-grouping.js?v=20260818-venuegroup1";
 import "./exhibition-gallery.js?v=20260818-gallery2";
-import "./exhibition-compact-loader.js?v=20260818-compact8";
+import "./exhibition-compact-loader.js?v=20260818-compact9";
 import "./presentation-normalizer.js?v=20260818-presentation3";
+import "./footer-credit.js?v=20260818-footer1";
