d11b783e30643bceacc12d38d1e2c2ea0a302822
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 12:51:17 -0400

    Bust footer cache for visible contact links

--- app/app.js
+++ app/app.js
@@ -2,4 +2,4 @@ import "./category-normalizer.js?v=20260818-categories3";
 import "./title-normalizer-bootstrap.js?v=20260818-title3";
 import "./app-core.js?v=20260818-exhibitions1";
 import "./static-exhibition-groups.js?v=20260818-staticgroups1";
-import "./footer-credit.js?v=20260818-footer1";
+import "./footer-credit.js?v=20260818-footer2";
