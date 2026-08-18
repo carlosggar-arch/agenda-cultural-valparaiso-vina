60d0ce666ca9425535b1a7bee317e3dc27d2ba90
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 10:22:37 -0400

    Make app base initialization deterministic

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
-import "./category-normalizer.js?v=20260818-categories3";
-import "./app-core.js?v=20260818-exhibitions1";
-import "./exhibition-venue-grouping.js?v=20260818-venuegroup1";
-import "./exhibition-gallery.js?v=20260818-gallery2";
-import "./exhibition-compact-loader.js?v=20260818-compact9";
-import "./presentation-normalizer.js?v=20260818-presentation3";
-import "./footer-credit.js?v=20260818-footer1";
+const baseReady = (async () => {
+  await import("./category-normalizer.js?v=20260818-categories3");
+  await import("./app-core.js?v=20260818-exhibitions1");
+  await import("./exhibition-venue-grouping.js?v=20260818-venuegroup1");
+  await import("./exhibition-gallery.js?v=20260818-gallery2");
+  await import("./exhibition-compact-loader.js?v=20260818-compact9");
+  await import("./presentation-normalizer.js?v=20260818-presentation3");
+  await import("./footer-credit.js?v=20260818-footer1");
+})();
+
+globalThis.__vivamosAppBaseReady = baseReady;
+await baseReady;
