eafe3323d87e4f211b096238aa02a5ae63b6092b
carlosggar-arch <carlosggar@gmail.com>
2026-08-18 12:19:31 -0400

    Load refined title normalizer bundle

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import "./category-normalizer.js?v=20260818-categories3";
-import "./title-normalizer-bootstrap.js?v=20260818-title1";
+import "./title-normalizer-bootstrap.js?v=20260818-title2";
 import "./app-core.js?v=20260818-exhibitions1";
 import "./static-exhibition-groups.js?v=20260818-staticgroups1";
 import "./footer-credit.js?v=20260818-footer1";
