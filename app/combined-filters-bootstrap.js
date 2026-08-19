9ae724c62b9424143adda061ef3c54a609bea4cb
carlosggar-arch <carlosggar@gmail.com>
2026-08-19 12:22:23 -0400

    Load filter fail-open guard after combined filters

--- app/combined-filters-bootstrap.js
+++ app/combined-filters-bootstrap.js
@@ -1,2 +1,3 @@
 import "./category-normalizer.js?v=20260818-categories3";
 await import("./combined-filters.js?v=20260818-public-taxonomy1");
+await import("./combined-filters-safety.js?v=20260819-gijon-visibility1");
