3741cd75269099d6e9dc2d51d67c05f40231022a
carlosggar-arch <carlosggar@gmail.com>
2026-08-19 10:41:17 -0400

    Apply session occurrence normalization before rendering

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import "./startup-stability.js?v=20260819-startup1";
 import "./event-data-corrections.js?v=20260819-rioja1";
 import "./category-normalizer.js?v=20260819-categories4";
 import "./title-normalizer-bootstrap.js?v=20260818-title3";
+import "./session-occurrence-normalizer.js?v=20260819-sessions1";
 import "./app-core.js?v=20260818-exhibitions1";
 import "./temporal-priority.js?v=20260819-temporal3";
 import "./static-exhibition-groups.js?v=20260818-staticgroups1";
