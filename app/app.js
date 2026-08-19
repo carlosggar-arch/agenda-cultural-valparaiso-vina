3e92e8ea68b42b87caa091fc657de2530f993a0c
carlosggar-arch <carlosggar@gmail.com>
2026-08-19 10:52:40 -0400

    Load program visibility policy before app core

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import "./event-data-corrections.js?v=20260819-rioja1";
 import "./category-normalizer.js?v=20260819-categories4";
 import "./title-normalizer-bootstrap.js?v=20260818-title3";
 import "./session-occurrence-normalizer.js?v=20260819-sessions1";
+import "./program-visibility-policy.js?v=20260819-programs1";
 import "./app-core.js?v=20260818-exhibitions1";
 import "./temporal-priority.js?v=20260819-temporal3";
 import "./static-exhibition-groups.js?v=20260818-staticgroups1";
